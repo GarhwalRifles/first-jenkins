fc6ca078b521082390c044e8fdb8f8fbf78631d7
GarhwalRifles <arawat0018@gmail.com>
2025-11-12 16:41:46 +0530

    second

--- src/main/java/com/RawatKafka/kafkatest/KafkaTestSamplesApplication.java
+++ src/main/java/com/RawatKafka/kafkatest/KafkaTestSamplesApplication.java
@@ -1,5 +1,7 @@
 package com.RawatKafka.kafkatest;
 
+import java.nio.channels.Pipe.SourceChannel;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,12 @@ public class KafkaTestSamplesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaTestSamplesApplication.class, args);
+		sit();
+	}
+	
+	//test
+	public static void sit() {
+		System.out.println("sit");
 	}
 
 }
